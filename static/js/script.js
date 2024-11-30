d8ab1c2d2f414067e96ba00fc5015d35127fbbcc
Ygal Idan -Linux <Ygal.Idan@gmail.com>
2024-11-30 21:41:08 +0200

    Limit 4 choose per item

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,28 @@
+let totalItems = 0;
+
 document.querySelectorAll('.increment').forEach(button => {
     button.addEventListener('click', () => {
         const input = button.previousElementSibling;
-        input.value = parseInt(input.value) + 1;
+        const currentValue = parseInt(input.value)
+
+        if (totalItems < 4) {
+            input.value = currentValue + 1;
+            totalItems++;
+        } else {
+            alert("You can only select up to 4 items.")
+        }
+        
     });
 });
 
 document.querySelectorAll('.decrement').forEach(button => {
     button.addEventListener('click', () => {
         const input = button.nextElementSibling;
-        if (parseInt(input.value) > 0) {
-            input.value = parseInt(input.value) - 1;
+        const currentValue = parseInt(input.value)
+
+        if (currentValue > 0) {
+            input.value = currentValue - 1;
+            totalItems--;
         }
     });
 });
